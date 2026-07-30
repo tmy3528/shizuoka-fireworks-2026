617caebe2c5fca5d9c6af9fb79f8ebcebf708b2a
tmy3528 <tmy3528@gmail.com>
2026-07-30 12:02:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -142,4 +142,4 @@ function updateCountdown() {
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const seconds = Math.floor((distance % (1000 * 60))
